09b0168d9dbf6f5bd13e43e9c8244d6378438124
kayo-b <kyo.bhz@gmail.com>
2022-08-01 11:21:47 -0300

    remaking factory funcion design

--- src/modules/projects.mjs
+++ src/modules/projects.mjs
@@ -1,30 +1,39 @@
 import { compareAsc, format, formatDistance, subDays} from 'date-fns';
+// import { forEach } from 'lodash';
 import newTask from "./tasks.mjs";
 
-const today = format(new Date(), 'dd/MM/yyyy HH:mm:ss')
+const today = format(new Date(), 'dd/MM/yyyy')
+
+function protoMethods() {
+    var protoMethods = {
+        setName(input){this.name = input},
+        getName(){return this.name},
+        setDesc(input){this.description = input},
+        setTask(task){this.tasks.push(task)},
+        deleteTask(taskIndex){this.tasks.splice(taskIndex,1)},
+        deleteAllTasks(){this.tasks = []},
+        // isToday(tasks){if(this.tasks.dueDate == today){today.push(task)}}
+    }
+    return protoMethods;
+}
 
 export default function createProject(name, desc) {
-    let protoMethods = {
-        setName(input){project.name = input},
-        getName(){return project.name},
-        setDesc(input){project.description = input},
-        setTask(taskName){project.tasks.push(newTask(taskName))},
-        deleteTask(taskIndex){project.tasks.splice(taskIndex,1)},
-        deleteAllTasks(){project.tasks = []},
-        isToday(task){if(task.dueDate == today){project[0].push(task)}}
-    }
-    let project = {
+    var project = {
         name: name,
         description: desc,
         tasks: [],
-        __proto__: protoMethods
+        todayArr:[],
+        isToday() {
+            for(let x = 0; x < this.tasks.length; x++) { 
+                if(this.tasks[x].dueDate === today) {
+                    this.todayArr.push(this.tasks[x])}}},
+        __proto__: protoMethods()
 
     }
     return project
 }
 
-
-
-
-
-
+var teste = createProject()
+teste.setTask(newTask("name","31/07/2022","notes teste", "low"))
+teste.isToday()
+console.log(teste.todayArr)
--- src/modules/tasks.mjs
+++ src/modules/tasks.mjs
@@ -1,19 +1,24 @@
-export default function newTask(name, note, date, level, status) {
+
+function protoMethods(){
 
     let protoMethods = {
-        setName(input){task.name = input},
-        getName(){return task.name},
-        setNote(input){task.note = input},
-        setDate(input){task.dueDate = input},
-        setPriority(input){task.priority = input}
+        setName(input){this.name = input},
+        getName(){return this.name},
+        setNote(input){this.note = input},
+        setDate(input){this.dueDate = input},
+        setPriority(input){this.priority = input}
     }
+    return protoMethods
+}
+
+export default function newTask(name, date, note, level, status) {
     var task = {
         name: name,
         note: note,
         dueDate: date,
         priority: level,
         status: status,
-        __proto__: protoMethods
+        __proto__: protoMethods()
         
     }
     return task;
